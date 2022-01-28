a5610d269c7caa5abfbb2bd60fb5bb3a4a14288a
TomaszKru <krukowski.tomsz@gmail.com>
2022-01-28 16:45:30 +0100

    log

--- store/views.py
+++ store/views.py
@@ -1,12 +1,19 @@
 import email
+import imp
 from itertools import product
+from multiprocessing import context
 import re
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from django.http import JsonResponse
+from django.contrib.auth.forms import UserCreationForm
 import json
 import datetime
 from .utils import cookieCart,cartData,guestOrder
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def store(request):
@@ -97,3 +104,43 @@ def processOrder(request):
 		)
 
 	return JsonResponse('Payment complete!', safe=False)
+
+def registerPage(request):
+	if request.user.is_authenticated:
+		return redirect('store')
+	else:
+		form = CreateUserForm()
+
+		if request.method == 'POST':
+			form=CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user=form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+				return redirect('login')
+
+		context={'form':form}
+		return render(request, 'store/register.html', context)
+
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('store')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password = request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('store')
+			else:
+				messages.info(request, 'Username or password is incorrect')
+			
+		context={}
+		return render(request, 'store/login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
